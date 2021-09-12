d8ed6be63a4ddb0eb6ebc3e0b740b55577dd0315
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-13 01:19:10 +0530

    interfaceBuilder app made using Xcode and swift

--- InterfaceBuilderBasics/InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/InterfaceBuilderBasics/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func changeTitle(_ sender: Any) {
+        mainLabel.text = "This app rocks!!!"
+    }
+    
 
 }
 
