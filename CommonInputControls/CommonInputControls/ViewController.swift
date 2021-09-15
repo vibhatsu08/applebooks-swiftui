95418ce61ac3be78aa8c0b3803596023364fd8fc
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-15 17:29:21 +0530

    CommonInputControls

--- CommonInputControls/CommonInputControls/ViewController.swift
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -11,6 +11,16 @@ class ViewController: UIViewController {
 
     @IBAction func buttonTapped(_ sender: UIButton){
         print("button was tapped")
+        
+        if toggle.isOn {
+            print("button is on!")
+        }
+        else{
+            print("button is off!")
+        }
+        
+        print("The slider is set to a value of \(slider.value)")
+        
     }
     @IBAction func switchToggled(_ sender: UISwitch) {
         if sender.isOn{
@@ -23,9 +33,25 @@ class ViewController: UIViewController {
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
     }
+    @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+        if let text = sender.text{
+            print(text)
+        }
+    }
+    
+    @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+        let touchLocation = sender.location(in: view)
+        print(touchLocation)
+    }
+    @IBOutlet var toggle: UISwitch!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button.addTarget(self, action: #selector(buttonTapped(_:)),
+           for: .touchUpInside)
     }
 
 
