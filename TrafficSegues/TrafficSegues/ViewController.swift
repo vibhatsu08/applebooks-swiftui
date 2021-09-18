81fbcc912cfa08a33611c67ae804e5c2e362d97e
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-19 02:54:32 +0530

    traffic segues

---
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Vedant Mistry on 19/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+}
+
