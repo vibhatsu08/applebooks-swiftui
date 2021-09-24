51848549f65b2bd7b307954b2ab8feb32864b50a
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-24 18:40:43 +0530

    personality quiz

--- PersonalityQuiz/PersonalityQuiz/Questions.swift
+++ PersonalityQuiz/PersonalityQuiz/Questions.swift
@@ -7,12 +7,37 @@
 
 import Foundation
 
-struct Question{
+struct Question { //made a structure that corresponds with the question
     var test: String
     var type: ResponseType
     var answers: [Answer]
 }
 
-enum ResponseType{
-    case single, multiple, ranged
+enum ResponseType {
+    case single, multiple, ranged //what type of answer is it, depending on the question type
+}
+
+struct Answer { //what would the answer contain
+    var text: String //the text of the string
+    var type: AnimalType //and the animal type a person is based on his answers given
+}
+
+enum AnimalType: Character { //enum of a animal type
+    case dog = "🐶", cat = "🐱", turtle = "🐢", rabbit = "🐰" //dog, cat, rabbit or turtle
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "you are incredibly outgoing, you surround yourself with the people you love and enjoy activities with your friends."
+            
+        case .cat:
+            return "mischievious yet mild tempered, you enjoy doing things on your own terms."
+            
+        case .turtle:
+            return "you love everything that's soft, you are healthy and full of energy."
+            
+        case .rabbit:
+            return "you are wise beyong your years, and you focus on the details, slow and steady wins the race."
+        }
+    }
 }
