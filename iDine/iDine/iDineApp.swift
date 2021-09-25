2154bd7e6710e3e97c72be0a20a1b290bf629cc0
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-25 22:02:57 +0530

    iDine app

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Vedant Mistry on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
