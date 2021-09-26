279ecb045cbb303e64766effc1659fea5af1613d
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-26 16:45:37 +0530

    iDine app

--- iDine/iDine/ItemDetail.swift
+++ iDine/iDine/ItemDetail.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ItemDetail: View {
     let item: MenuItem
+    @EnvironmentObject var order: Order
     
     var body: some View {
         VStack {
@@ -25,6 +26,10 @@ struct ItemDetail: View {
             }
             Text(item.description)
                 .padding()
+            Button("Order This"){
+                order.add(item: item)
+            }
+            .font(.headline)
             Spacer()
         }
         .navigationTitle(item.name)
@@ -33,6 +38,9 @@ struct ItemDetail: View {
 
 struct ItemDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ItemDetail(item: MenuItem.example)
+        NavigationView{
+            ItemDetail(item: MenuItem.example)
+                .environmentObject(Order())
+        }
     }
 }
