1eee37ddc0335e0a7d6da3cb4e4723dbd21096f4
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-28 17:14:34 +0530

    iDine app

--- iDine/iDine/CheckoutView.swift
+++ iDine/iDine/CheckoutView.swift
@@ -14,7 +14,7 @@ struct CheckoutView: View {
     let paymentTypes = ["Cash", "Credit card", "iDine points"]
     
     var body: some View {
-        VStack{
+        Form{
             Section{
                 Picker("How do you want to pay?", selection: $paymentType){
                     ForEach(paymentTypes, id: \.self){
